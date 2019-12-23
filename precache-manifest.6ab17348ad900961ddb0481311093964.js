8674d476fccb10cd7266df17b004791b69c9710a
Vinod Kumar <vkvinodk72@gmail.com>
2019-12-23 10:40:22 +0530

    Updates

---
+++ precache-manifest.6ab17348ad900961ddb0481311093964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc43da0aaa90c33c6a3c3e843041d4a",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "d87890fa97153c7755bb",
+    "url": "/monster/static/css/main.cd67db1b.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monster/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "d87890fa97153c7755bb",
+    "url": "/monster/static/js/main.7ab7407f.chunk.js"
+  },
+  {
+    "revision": "10f33502b98129ee985a",
+    "url": "/monster/static/js/runtime-main.5ea53213.js"
+  }
+]);
